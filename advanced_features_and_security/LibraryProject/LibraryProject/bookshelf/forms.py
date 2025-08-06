37aa08b6a99ad27b499ef4ec8b8ecd458251abb4
7777289 <innocentbenbenimana@gmail.com>
2025-08-06 15:18:03 +0300

    Add ExampleForm in forms.py for Book model

---
+++ advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,9 @@
+# LibraryProject/bookshelf/forms.py
+
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description', 'publication_date']
