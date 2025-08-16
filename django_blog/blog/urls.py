3c55142112d777b205afd931ca5b38d42dba622f
7777289 <innocentbenbenimana@gmail.com>
2025-08-16 18:00:47 +0300

    update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 from .views import (
     PostListView, PostDetailView, PostCreateView,
     PostUpdateView, PostDeleteView,
-    CommentCreateView, CommentUpdateView, CommentDeleteView ,SearchResultsView
+    CommentCreateView, CommentUpdateView, CommentDeleteView ,SearchResultsView,PostByTagListView
 )
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -23,10 +24,14 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    # Comment URLs (corrected)
+    # Comment URLs
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-    path('tags/<slug:slug>/', views.posts_by_tag, name='posts-by-tag'),
+
+    # Tags ✅ must match exactly
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
+
+    # Search
     path('search/', SearchResultsView.as_view(), name='post-search'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Comment, Tag
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, PostForm, CommentForm
+from taggit.models import Tag
 
 # ---------------- HOME ---------------- #
 
@@ -172,3 +173,12 @@ class SearchResultsView(ListView):
         ctx['q'] = self.request.GET.get('q', '')
         return ctx
 
+
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs.get("tag_slug"))
