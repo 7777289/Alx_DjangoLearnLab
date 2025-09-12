6c8afea2b87efc2f7e03685074bf377ae9c2637b
7777289 <innocentbenbenimana@gmail.com>
2025-09-12 20:14:54 +0300

    updates

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='actions')
+    verb = models.CharField(max_length=255)
+    
+    # GenericForeignKey for the target object
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    target = GenericForeignKey('content_type', 'object_id')
+    
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.actor.username} {self.verb} {self.target}'
+
+    class Meta:
+        ordering = ('-timestamp',)
